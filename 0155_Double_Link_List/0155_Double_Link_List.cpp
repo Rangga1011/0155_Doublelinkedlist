afbdc23b371a605932edfab0d5c843eb67a7dd25
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-05-02 11:23:47 +0700

    Menambahkan input dan display if start

--- 0155_Double_Link_List/0155_Double_Link_List.cpp
+++ 0155_Double_Link_List/0155_Double_Link_List.cpp
@@ -22,6 +22,11 @@ void addNode() {
 	//insert the new node in the list
 	if (START == NULL || newNode->noMhs <= START->noMhs) {  // step 2 insert the new node
 
+		if (START != NULL && newNode->noMhs == START->noMhs) {
+			cout << "\033[31Duplicate roll numbers not allowed\033[0m" << endl;
+			return;
+		}
+
 
 	}
 	
