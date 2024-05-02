9d7a1da338e073a242dafad005ab0f34630e3285
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-05-02 11:12:37 +0700

    Menambahkan step 1 create a new node

--- 0155_Double_Link_List/0155_Double_Link_List.cpp
+++ 0155_Double_Link_List/0155_Double_Link_List.cpp
@@ -12,6 +12,13 @@ struct Node
 
 Node* START = NULL;
 
+void addNode() {
+	Node* newNode = new Node(); // step 1 create a new node
+	cout << "\nEnter the roll number of the student: ";
+	
+
+}
+
 
 
 
